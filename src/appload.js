44adc4650cdeacbb78887f3bc425f01f138cfd25
riven <4771215@qq.com>
2024-11-17 17:33:42 +0800

    nav bar hide

---
+++ src/appload.js
@@ -0,0 +1,8 @@
+import { SplashScreen } from '@capacitor/splash-screen';
+import { StatusBar } from '@capacitor/status-bar';
+
+document.addEventListener('DOMContentLoaded', () => {
+  StatusBar.hide();
+  SplashScreen.hide();
+})
+
